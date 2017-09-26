dae72d49ed29dea511edb271ff696e9008797fce
Chokri Jobs <chokri.jobs@gmail.com>
2017-09-26 18:14:14 +0100

    add style b 4

--- src/Sypress/Templates/Front/layout.php
+++ src/Sypress/Templates/Front/layout.php
@@ -2,6 +2,7 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <link href="assets/css/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <title><?php $view['slots']->output('title', 'Hello Application') ?></title>
 </head>
 <body>
