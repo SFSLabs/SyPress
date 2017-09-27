d1b5aefeab45ee96927322fc9583adf80f7ceabf
sinteg <selmi.chokri@gmail.com>
2017-09-27 12:05:27 +0100

    update style

--- src/Sypress/Templates/Front/layout.php
+++ src/Sypress/Templates/Front/layout.php
@@ -2,10 +2,13 @@
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <link href="assets/css/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
+    <link href="assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" />
     <title><?php $view['slots']->output('title', 'Hello Application') ?></title>
 </head>
 <body>
 <?php $view['slots']->output('_content') ?>
+<!-- JS Block -->
+<script src="assets/bootstrap/js/bootstrap.min.css"></script>
+<!--End JS Block -->
 </body>
 </html>
